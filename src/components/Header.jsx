9e989aeaa7f046ac19910229fa4d4d21b1b758e9
ne518 <nehasehrawat797@gmail.com>
2025-07-13 12:47:41 +0530

    Initial commit - FareCast full stack project

---
+++ src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="p-4 bg-blue-600 text-white text-xl">FareCast</header>;
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="p-4">Welcome to FareCast - Predict the cheapest flights!</div>;
+}
